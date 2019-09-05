a2b21993f38255b1fba0cf258c793e71a1aec5ab
Arnaldo <41029115+arnjr1986@users.noreply.github.com>
2019-09-05 20:07:17 -0300

    Dia-Dos-Namorados-2019
    Programa singelo e feito com coração, só pra demonstrar que sei declarar mais que variáveis...
    obs: Fiz um vídeo no celular do programa rodando no QPython e coloquei no Status do Whatsapp na data 12-06-2019

---
+++ Lilian.py
@@ -0,0 +1,12 @@
+#DECLARANDO AMOR EM PYTHON
+ela = (input('Qual é o nome dela? '))
+ele = (input('Qual é o nome dele? '))
+print('{} você é o "achado" do {}!'.format(ela,ele))
+
+recado = (input('\nEle tem um recado pra você: '))
+
+
+print('{}, o {} {} '.format(ela,ele,recado))
+
+print('\nTe quero, hoje...\nTe quero, amanhã...\ne depois e depois e depois....\nVocê é meu "Deus me livre, Mas quem me dera...', end='')
+print('\nTe Amo!\nFeliz dia dos Namorados 2019 ;)')
